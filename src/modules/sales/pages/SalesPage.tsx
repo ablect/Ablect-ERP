f87ea60e41a99102275e41434597e0315c8097c1
ablect <olukitibioluwatobaisrael@gmail.com>
2026-08-09 00:14:50 +0100

    feat(sales): switch sales page to modern POS workspace

--- src/modules/sales/pages/SalesPage.tsx
+++ src/modules/sales/pages/SalesPage.tsx
@@ -1,47 +1,5 @@
-import { useState } from "react";
-
-import SectionTitle from "../../../components/ui/SectionTitle";
-
-import SalesOverview from "../components/SalesOverview";
-import NewSaleButton from "../components/NewSaleButton";
-import EmptySales from "../components/EmptySales";
-import SaleForm from "../components/SaleForm";
-import SalesTable from "../components/SalesTable";
-
-import { useSales } from "../hooks/useSales";
+import ModernSalesPOS from "../components/ModernSalesPOS";
 
 export default function SalesPage() {
-  const [showForm, setShowForm] =
-    useState(false);
-
-  const { sales } = useSales();
-
-  return (
-    <div className="space-y-8">
-      <SectionTitle
-        title="Sales"
-        subtitle="Manage all customer sales."
-      />
-
-      <SalesOverview />
-
-      <div className="flex justify-end">
-        <NewSaleButton
-          onClick={() =>
-            setShowForm(true)
-          }
-        />
-      </div>
-
-      {showForm && (
-        <SaleForm />
-      )}
-
-      {sales.length === 0 ? (
-        <EmptySales />
-      ) : (
-        <SalesTable sales={sales} />
-      )}
-    </div>
-  );
+  return <ModernSalesPOS />;
 }
