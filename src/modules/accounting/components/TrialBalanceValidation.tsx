485d8ea3192cebbb0ecb22536da70f031f35909e
ablect <olukitibioluwatobaisrael@gmail.com>
2026-08-09 17:43:14 +0100

    fix(accounting): use actual trial balance hook filename casing

--- src/modules/accounting/components/TrialBalanceValidation.tsx
+++ src/modules/accounting/components/TrialBalanceValidation.tsx
@@ -1,55 +1,11 @@
-import {
+import { useTrialBalanceValidation } from "../hooks/useTrialbalanceValidation";
 
-useTrialBalanceValidation
-
-}
-
-from "../hooks/useTrialBalanceValidation";
-
-export default function TrialBalanceValidation(){
-
-const{
-
-balanced,
-
-difference,
-
-}=
-
-useTrialBalanceValidation();
-
-return(
-
-<div>
-
-<p>
-
-{
-
-balanced
-
-?
-
-"Trial Balance Balanced"
-
-:
-
-"Trial Balance Out of Balance"
-
-}
-
-</p>
-
-<p>
-
-Difference:
-
-₦{difference.toLocaleString()}
-
-</p>
-
-</div>
-
-);
-
+export default function TrialBalanceValidation() {
+  const { balanced, difference } = useTrialBalanceValidation();
+  return (
+    <div>
+      <p>{balanced ? "Trial Balance Balanced" : "Trial Balance Out of Balance"}</p>
+      <p>Difference: ₦{difference.toLocaleString()}</p>
+    </div>
+  );
 }
