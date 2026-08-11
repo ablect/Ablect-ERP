9b6586d9bff4b8a0c7fd9d9e27a387e1e9efa7a3
ablect <olukitibioluwatobaisrael@gmail.com>
2026-08-11 19:28:23 +0100

    feat: route application through commercial system gate

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./styles/theme.css";
-import App from "./App";
 import { AuthProvider } from "./auth/AuthContext";
+import SystemGate from "./SystemGate";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <SystemGate />
     </AuthProvider>
   </StrictMode>,
 );
